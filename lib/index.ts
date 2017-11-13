1d65dcf81faa21dc000a6e9f9291e6de1ae23106
Francesco Stasi <f.stasi@me.com>
2017-11-13 13:39:23 +0100

    package updates

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,9 @@
-export { FileDropModule as  Ng2FileDropModule } from './module/file-drop.module';
+export { FileDropModule } from './module/file-drop.module';
 
-export { FileDropDirective as Ng2FileDropDirective } from './directives/file-drop.directive';
+export { FileDropDirective } from './directives/file-drop.directive';
 
-export { RejectionReasons as Ng2FileDropRejections } from './properties/rejection-reasons';
+export { RejectionReasons } from './properties/rejection-reasons';
 
-export { AcceptedFile as Ng2FileDropAcceptedFile } from './dropped-files/accepted-file';
-export { RejectedFile as Ng2FileDropRejectedFile } from './dropped-files/rejected-file';
-export { DroppedFiles as Ng2FileDropFiles } from './dropped-files/dropped-files';
+export { AcceptedFile } from './dropped-files/accepted-file';
+export { RejectedFile } from './dropped-files/rejected-file';
+export { DroppedFiles } from './dropped-files/dropped-files';
